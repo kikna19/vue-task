e6cb06b817897505d4c0b173b709dabb6671712d
kikna19 <vazhiko199@gmail.com>
2022-09-24 17:57:02 +0400

    vue task

--- src/components/HeaderApp.vue
+++ src/components/HeaderApp.vue
@@ -1,18 +1,22 @@
 <template>
-  <div class="container-fluid main">
-    <h1 class="col-4">favourite cars</h1>
-    <div class="col d-flex justify-content-end">
-      <a>home</a>
-      <a>gallery</a>
-      <a>about us</a>
-      <a>contact</a>
-    </div>
-    <i @click="menu" class="bi bi-list-ul"></i>
-    <div v-if="drop" class="container-fluid d-flex flex-column drop">
-      <a>home</a>
-      <a>gallery</a>
-      <a>about us</a>
-      <a>contact</a>
+  <div class="container-fluid  main pt-2">
+    <div class="row d-flex align-items-center">
+      <div class="col-4 d-flex  align-items-">
+        <h1>favourite cars</h1>
+      </div>
+      <div class="col-8 d-flex justify-content-end align-items-center">
+        <a>home</a>
+        <a>gallery</a>
+        <a>about us</a>
+        <a>contact</a>
+      </div>
+      <i @click="menu" class="bi bi-list-ul"></i>
+      <div v-if="drop" class="container-fluid d-flex flex-column drop">
+        <a>home</a>
+        <a>gallery</a>
+        <a>about us</a>
+        <a>contact</a>
+      </div>
     </div>
   </div>
 </template>
@@ -31,9 +35,9 @@ export default {
     menu() {
       this.drop = !this.drop;
       if (this.drop)
-        document.body.style.overflow = 'hidden'
+        document.body.style.overflowY = 'hidden'
       else
-        document.body.style.overflow = 'scroll'
+        document.body.style.overflowY = 'scroll'
     }
   }
 
@@ -46,20 +50,18 @@ export default {
 @import url('https://fonts.googleapis.com/css2?family=Rubik+Dirt&display=swap');
 $border-color: #b964f3;
 
-@mixin flex($align, $justify, $dir) {
-  display: flex;
-  align-items: $align;
-  justify-content: $justify;
-  flex-direction: $dir;
-}
-
 
 .main {
-  @include flex(center, space-between, row);
+  display: inline-block;
   height: 5rem;
-  padding: 0 5rem 0 5rem;
+  padding: 0 4.3rem 0 4.3rem;
   color: white;
   border-bottom: .1rem solid $border-color;
+
+  .row {
+    height: 80% !important;
+  }
+
 
   h1 {
     font-family: 'Arizonia', cursive;
@@ -70,7 +72,7 @@ $border-color: #b964f3;
     margin-left: 3rem;
     font-family: 'Rubik Dirt', cursive;
     text-transform: capitalize;
-    font-size: 1.2rem;
+    font-size: 1.1rem;
   }
 
   i {
@@ -87,7 +89,6 @@ $border-color: #b964f3;
     height: calc(100% - 5rem);
     background: rgb(32, 33, 36);
     z-index: 1000;
-    transition: 1s all ease-in-out;
 
     a {
       margin: 2rem 0 0 0;
@@ -98,7 +99,7 @@ $border-color: #b964f3;
 @media (max-width:920px) {
   .main {
     h1 {
-      font-size: 1.8rem;
+      font-size: 1.7rem;
     }
 
     a {
@@ -110,15 +111,20 @@ $border-color: #b964f3;
 
 @media (max-width:750px) {
   .main {
+    h1 {
+      font-size: 1.5rem;
+    }
+
     a {
       margin-left: 1rem;
+      font-size: 1rem;
     }
   }
 }
 
 @media (max-width:680px) {
   .main {
-    .col {
+    .col-8 {
       display: none;
 
       a {
@@ -128,10 +134,14 @@ $border-color: #b964f3;
 
     .col-4 {
       width: auto;
+      margin-top: .6rem;
     }
 
     i {
+      position: absolute;
+      left: calc(100% - 6.9rem);
       display: block;
+      width: auto;
     }
   }
 }
@@ -139,10 +149,14 @@ $border-color: #b964f3;
 
 @media (max-width:550px) {
   .main {
-    padding: 0 2rem 0 2rem;
+    padding: 0 1.3rem 0 1.3rem;
 
     .drop {
       padding-left: 2rem;
+    }
+
+    i {
+      left: calc(100% - 3.9rem);
     }
   }
 }
